cecefcf6580e6c20de4babdd7b31b4c5ff63b532
Lennix <lennix@mc-wow.eu>
2012-04-06 07:39:18 +0200

    Irgendein quatsch

--- zp_characters/character_tutorial.sql
+++ zp_characters/character_tutorial.sql
@@ -11,5 +11,5 @@ CREATE TABLE `character_tutorial` (
   `tut6` int(11) unsigned NOT NULL DEFAULT '0',
   `tut7` int(11) unsigned NOT NULL DEFAULT '0',
   PRIMARY KEY (`account`)
-) ENGINE=InnoDB AUTO_INCREMENT=30157 DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC COMMENT='Player System';
+) ENGINE=InnoDB AUTO_INCREMENT=30410 DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC COMMENT='Player System';
 /*!40101 SET character_set_client = @saved_cs_client */;
